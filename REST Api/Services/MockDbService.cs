5723a8ca8a942392dd2533f8d92d0c1bfb5ae597
Abduganeoff <abduganeoff@gmail.com>
2021-02-28 23:56:18 +0100

    MockDbService added

---
+++ REST Api/Services/MockDbService.cs
@@ -0,0 +1,27 @@
+﻿using REST_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_Api.Services
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student{IdNumber=1, FirstName="Rizo", SecondName="Abduganeoff", IndexNumber="s2020"},
+                new Student{IdNumber=2, FirstName="Muhammad", SecondName="Rizo", IndexNumber="s2021"},
+                new Student{IdNumber=3, FirstName="Jason", SecondName="Stetham", IndexNumber="s2022"}
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students
+        }
+    }
+}
